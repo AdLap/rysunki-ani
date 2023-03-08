2d404d8dfd0b46f282b679d31f36e3159dae995a
Adam Łapacz <a.lapacz@global4net.com>
2023-03-08 13:58:36 +0100

    ANNGAL-5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { GalleryProvider } from './context/Gallery.context'
 import { Routes, Route, useLocation } from 'react-router'
 import { Normalize } from 'styled-normalize'
@@ -20,35 +19,24 @@ const App = () => {
 	const location = useLocation()
 
 	return (
-		<HelmetProvider>
-			<GalleryProvider>
-				<ThemeProvider theme={theme}>
-					<Helmet>
-						<title>Galeria obrazków Ani</title>
-						<link
-							rel='stylesheet'
-							href='<link rel="preconnect" href="https://fonts.googleapis.com">
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">'
-						/>
-					</Helmet>
-					<Normalize />
-					<GlobalStyle />
-					<Header />
-					<NavBar />
-					<AnimatePresence>
-						<Routes location={location} key={location.key}>
-							<Route path='/' element={<Home />} />
-							<Route path='/plotna' element={<OnCanvas />} />
-							<Route path='/szkice' element={<Sketch />} />
-							<Route path='/kamienie' element={<Stone />} />
-							<Route path='/kontakt' element={<Contact />} />
-						</Routes>
-					</AnimatePresence>
-					<Footer />
-				</ThemeProvider>
-			</GalleryProvider>
-		</HelmetProvider>
+		<GalleryProvider>
+			<ThemeProvider theme={theme}>
+				<Normalize />
+				<GlobalStyle />
+				<Header />
+				<NavBar />
+				<AnimatePresence>
+					<Routes location={location} key={location.key}>
+						<Route path='/' element={<Home />} />
+						<Route path='/plotna' element={<OnCanvas />} />
+						<Route path='/szkice' element={<Sketch />} />
+						<Route path='/kamienie' element={<Stone />} />
+						<Route path='/kontakt' element={<Contact />} />
+					</Routes>
+				</AnimatePresence>
+				<Footer />
+			</ThemeProvider>
+		</GalleryProvider>
 	)
 }
 
